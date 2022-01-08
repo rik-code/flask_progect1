63feee14bd6570bcaaca1c2ebb2b3189a38b05f0
rik-code <topelven1234567890@gmail.com>
2022-01-08 21:10:07 +0300

    Recent changes on the site 😌

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/')  # что будет, если зайти на главную страницу сайта  vk.com/ <-
+def index():
+    context = {'title': 'My first site'}  # набор элементов, передаваемый в шаблон сайта
+    return render_template('index.html', context=context)  # при переходе на главную страницу, показать шаблон index.html
 
+
+@app.route('/contacts')
+def contacts():
+    context = {'title': 'Contacts page'}  # набор элементов, передаваемый в шаблон сайта
+    return render_template('contacts.html', context=context)
 
 if __name__ == '__main__':
     app.run()
