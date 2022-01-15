ed567160753a3ecdf1eec8123e4707cafeaae993
rik-code <topelven1234567890@gmail.com>
2022-01-15 21:11:57 +0300

    Adding a form to the contact page📝

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 
 @app.route('/')  # Что будет, если зайти на главную страницу сайта
 def index():
@@ -7,7 +7,17 @@ def index():
     return render_template('index.html', context=context)  # При переходе на главную страницу, показать шаблон index.html
 
 
-@app.route('/contacts')
+@app.route('/contacts', methods=['GET', 'POST'])  # Учу функцию воспринимать методы отправки формы
 def contacts():
     context = {'title': 'Contacts page'}  # Набор элементов, передаваемый в шаблон сайта
-    return render_template('contacts.html', context=context)
+    user = None
+    if request.method == 'POST':  # Если нажали кнопку отправки формы
+        form = request.form  # Связываю данные формы с функцией
+        user = {  # Записываю данные из полей формы в словарь
+            'name': form.get('name'),
+            'surname': form.get('surname'),
+            'phone': form.get('p_num')
+        }
+        return render_template('contacts.html', context=context, user=user)
+    else:
+        return render_template('contacts.html', context=context)
